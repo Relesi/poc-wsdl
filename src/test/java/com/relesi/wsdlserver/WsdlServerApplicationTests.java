2e53f50e94f1275aacf1eddc77b3be333309550a
relesi <renatolessa.2011@hotmail.com>
2021-08-11 11:44:14 -0300

    first commit to poc wsdl for integration

---
+++ src/test/java/com/relesi/wsdlserver/WsdlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relesi.wsdlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
